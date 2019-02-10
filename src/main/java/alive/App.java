eb608a14cf2538d769403cd50ef9f8f6fefc1541
alive <alkyalive@gmail.com>
2019-02-10 22:53:16 +0400

    Initial

---
+++ src/main/java/alive/App.java
@@ -0,0 +1,16 @@
+package alive;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        HelloWorld test = new HelloWorld();
+        test.run();
+
+        System.out.println( "Hello World!" );
+    }
+}
