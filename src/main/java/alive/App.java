7ff4b16fd7f0283aed3678a2cd256df16c1f2d01
alive <alkyalive@gmail.com>
2019-02-12 22:03:20 +0400

    WIP: ortho projection with points

--- src/main/java/alive/App.java
+++ src/main/java/alive/App.java
@@ -13,6 +13,11 @@ public class App
 {
     private AppWindow appWindow;
 
+    private int worldWidth = 100;
+    private int worldHeight = 100;
+
+
+
     public void run()
     {
         appWindow = new AppWindow("3 body problem", 300, 300);
@@ -41,12 +46,30 @@ public class App
     public void loop()
     {
         // Set the clear color
-        glClearColor(1.0f, 1.0f, 0.0f, 0.0f);
+        glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
+
+        glMatrixMode(GL_PROJECTION_MATRIX);
+        glLoadIdentity();
+        glOrtho(0, worldWidth, worldHeight, 0, 1, -1);
+
+        glPointSize(2.0f);
 
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
         while ( !appWindow.shouldClose() ) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
+            glBegin (GL_POINTS);
+
+            glVertex3f (10, 10, 0);
+            glVertex3f (10, worldHeight - 10, 0);
+            glVertex3f (worldWidth - 10, 10, 0);
+            glVertex3f (worldWidth - 10, worldHeight - 10, 0);
+
+            // Center dot
+            glVertex3f (50, 50, 0);
+
+            glEnd ();
 
             appWindow.loopHandler();
         }
